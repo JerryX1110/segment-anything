5fe1516b3607b74d47a442e337007dff8587ede0
JerryX <65257938+JerryX1110@users.noreply.github.com>
2023-04-18 16:01:59 +0800

    Update image_encoder.py

--- segment_anything/modeling/image_encoder.py
+++ segment_anything/modeling/image_encoder.py
@@ -106,6 +106,9 @@ class ImageEncoderViT(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.patch_embed(x)
         if self.pos_embed is not None:
+            self.pos_embed = self.pos_embed.permute(0,3,1,2)
+            self.pos_embed = torch.nn.functional.interpolate(self.pos_embed, (x.size()[1],x.size()[2]))
+            self.pos_embed = self.pos_embed.permute(0,2,3,1)
             print("self.pos_embed.size()",self.pos_embed.size())
             print("x.size()",x.size())
             x = x + self.pos_embed
